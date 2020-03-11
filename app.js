125779e0b92902b906aabad1fab84148ec12df68
Stephen Mufutau-Adams <43826715+stephen-m-a@users.noreply.github.com>
2020-03-11 15:18:21 +0000

    Add files via upload
    Changes App.js

--- app.js
+++ app.js
@@ -21,19 +21,24 @@ app.get('/signup', function(req, res){
     console.log('request was made on: /signup ');
 })
 
-
-
-
-
-
-
-
 // fire controller - boom boom
 
 actionController(app);
 
-
-
 app.listen(8080);
 
 console.log("Yo Dog, We do be listening to Localhost: 8080")
+
+//Google ID
+const GoogleAuth = require('simple-google-openid');
+// you can put your client ID here
+app.use(GoogleAuth(process.env.GOOGLE_CLIENT_ID));
+
+// return 'Not authorized' if we don't have a user
+app.use('/api', GoogleAuth.guardMiddleware());
+
+app.get('/api/hello', (req, res) => {
+  res.send('Hello ' + (req.user.displayName || 'user without a name') + '!');
+
+  console.log('successful authenticated request by ' + req.user.emails[0].value);
+});
